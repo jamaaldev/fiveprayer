d237e9b08b9ddb864a41871c14fbb57d8679f9d5
jamaalhassan <kabarojamaal@gmail.com>
2022-08-06 23:50:02 +0100

    added:table front

--- php/classes/enqueue/FivePrayer_enqueue.php
+++ php/classes/enqueue/FivePrayer_enqueue.php
@@ -3,6 +3,13 @@
 class FivePrayer_enqueue{
     function __construct(){
         add_action( 'admin_enqueue_scripts', array($this,'fiveprayer_admin_enqueue_scripts'));
+        add_action( 'wp_enqueue_scripts', array($this,'register_wprs'),99 );
+
+
+    }
+
+    function register_wprs(){
+        wp_enqueue_style( 'fiveprayer-style', plugin_dir_url( __FILE__ ) . '../../../build/index.css' ); // plugin_dir_url marka rabto inaad usheegto mesha fileka ku yaalo,sida css iyo js
 
     }
 
@@ -18,7 +25,7 @@ function fiveprayer_admin_enqueue_scripts() {
 		$valid_pages = array("fiveprayer");
         $page = (isset($_GET['page'])) ? $_GET['page'] : 0;
         if($page === 'fiveprayer'){
-          
+            
         wp_enqueue_style( 'fiveprayer-style', plugin_dir_url( __FILE__ ) . '../../../build/index.css' ); // plugin_dir_url marka rabto inaad usheegto mesha fileka ku yaalo,sida css iyo js
         wp_enqueue_script( 'fiveprayer-script', plugin_dir_url( __FILE__ ) . '../../../build/index.js', array( 'wp-element' ), '1.0.0', true );
         wp_localize_script( 'fiveprayer-script', 'prayerData', array(
