2ab362378e0896c4ed16d20f464d83cdbfd52668
jamaalhassan <kabarojamaal@gmail.com>
2022-08-29 04:08:52 +0100

    added: csv to json

--- src/admin/components/elements/BoxModel.tsx
+++ src/admin/components/elements/BoxModel.tsx
@@ -8,37 +8,80 @@ export interface IBoxModelProps {
 }
 
 export function BoxModel(props: IBoxModelProps) {
+    const [filename, SetfileName] = React.useState<string>('');
     const refs = React.useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
+    const FileUpload = (file) => {
+        console.log("🚀 ~ file: BoxModel.tsx ~ line 13 ~ FileUpload ~ file", file.target.files[0])
 
-    console.log("🚀 ~ file: BoxModel.tsx ~ line 10 ~ BoxModel ~ refs", props.showPopup)
-    if (refs?.current?.style) {
-
-        refs.current.style.display = props.showPopup;
+        SetfileName(file.target.files[0].name)
+        ConvertCSVToJSON(file.target.files[0])
     }
-    const ModelPoup = () => {
-        refs.current.style.display = "none";
+    const ConvertCSVToJSON = (csv) => {
+        // reading csv file
+        const reader = new FileReader();
+        reader.readAsText(csv);
+        reader.onload = () => { 
+            const text = reader.result;
+            let lines = [];
+            const linesArray = text?.split('\n');
+            // for trimming and deleting extra space 
+            linesArray.forEach((e: any) => {
+                const row = e.replace(/[\s]+[,]+|[,]+[\s]+/g, ',').trim();
+                lines.push(row);
+            });
 
-        props.SetShow(false);
+            // for removing empty record
+            lines.splice(lines.length - 1, 1);
+            const result = [];
+            const headers = lines[0].split(",");
 
-    }
-    return (
-        <div className='fp__model'>
+            for (let i = 1; i < lines.length; i++) {
 
-            <div ref={refs} id="myModal" className="modal">
+                const obj = {};
+                const currentline = lines[i].split(",");
+
+                for (let j = 0; j < headers.length; j++) {
+                    obj[headers[j]] = currentline[j];
+                }
+                result.push(obj);
+            }
+            //return result; //JavaScript object
+            // return JSON.stringify(result); //JSON
+            console.log("🚀 ~ file: BoxModel.tsx ~ line 52 ~ ConvertCSVToJSON ~ result", result)
+        }
+
+    };
 
 
-                <div className="modal-content">
-                    <div onClick={ModelPoup} className="close">&times;</div>
+console.log("🚀 ~ file: BoxModel.tsx ~ line 10 ~ BoxModel ~ refs", props.showPopup)
+if (refs?.current?.style) {
+
+    refs.current.style.display = props.showPopup;
+}
+const ModelPoup = () => {
+    refs.current.style.display = "none";
+
+    props.SetShow(false);
+
+}
+return (
+    <div className='fp__model'>
+
+        <div ref={refs} id="myModal" className="modal">
 
 
-                    <input type="file" id="actual-btn" hidden />
+            <div className="modal-content">
+                <div onClick={ModelPoup} className="close">&times;</div>
 
-                    <label for="actual-btn">Choose File</label>
 
-                    <span id="file-chosen">No file chosen</span>
-                </div>
+                <input onChange={FileUpload} type="file" id="actual-btn" hidden />
 
+                <label htmlFor="actual-btn">Choose File</label>
+
+                <span id="file-chosen">{filename ? filename : 'No file chosen'}</span>
             </div>
+
         </div>
-    );
+    </div>
+);
 }
