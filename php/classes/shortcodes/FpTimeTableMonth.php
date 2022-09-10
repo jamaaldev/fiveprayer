fc3b8c4442bd30a94f7ffa2b8e32d7075838989c
jamaaldev <kabarojamaal@gmail.com>
2022-09-10 10:09:25 +0100

    added:diff month to print still need fix

--- php/classes/shortcodes/FpTimeTableMonth.php
+++ php/classes/shortcodes/FpTimeTableMonth.php
@@ -5,17 +5,21 @@
  {
   public function __construct()
   {
-   add_action('init', array($this, 'registerShortcodes'));
-   add_action('wp_enqueue_scripts', array($this,'tablemonth'));
-
-   require_once(plugin_dir_path( __FILE__ ) . '../module/GenerateTimeTableMonthly.php');
-
-
-  }
-
-  public function tablemonth()
-{
+      add_action('init', array($this, 'registerShortcodes'));
+      add_action('wp_enqueue_scripts', array($this,'tablemonth'));
+      
+      
+      require_once(plugin_dir_path( __FILE__ ) . '../module/GenerateTimeTableMonthly.php');
+      
+      
+    }
+    
+    public function tablemonth()
+    {
+    wp_localize_script('table_script', 'table_ajax_url', array('ajax_url' => admin_url('admin-ajax.php')));
     wp_enqueue_style('tablemonth', plugin_dir_url(__FILE__) . './tablemonth.css', true);
+    wp_enqueue_script('table_script',  plugin_dir_url(__FILE__)  . '../../../build/index.js', '1.0.0', true);
+
   
 }
 
@@ -28,19 +32,35 @@
   public function fpTimetableMonth($atts)
   {
    global $wpdb;
+   $month;
+   if (isset($_COOKIE["fcookie"])) 
+        $month = $_COOKIE["fcookie"]; 
+else 
+echo "Cookie Not Set";
    $mydate    = wp_date("j", null, $timezone = null);
-      $monthdate =  wp_date("n", null, $timezone = null);
+      $monthdate =  $month ? $month : wp_date("n", null, $timezone = null) ;
       $yeardate = wp_date("Y", null, $timezone = null);
-   $prayersettingmeta = $wpdb->get_results("SELECT * FROM wp_fp_timetable WHERE YEAR(Date) = $yeardate  AND MONTH(Date) = $monthdate ");
+   $prayersettingmeta =  $wpdb->get_results("SELECT * FROM wp_fp_timetable WHERE YEAR(Date) = $yeardate  AND MONTH(Date) = $monthdate ");
     new GenerateTimeTableMonthly($prayersettingmeta);
    ob_start();
 
-  if ($prayersettingmeta) {?>
+
+  if ($prayersettingmeta ) {?>
 
             <table  id="divToPrint" class='FP_TablePrayer_'>
                 <thead class='waa'>
                     <tr class="tbmonth">
-                        <th>  <input class='noPrint' type="button" value="print" onclick="PrintDiv('divToPrint');" /></th>
+                        <th>  
+                             <form>
+                                <select name="country" onchange="getMonth(this)">
+                                    <option value="" disabled selected>--select--</option>
+                                    <option value="1">January</option>
+                                    <option value="2">February</option>
+                                    <option value="3">March</option>
+                                </select>
+                            </form>
+                            <input class='noPrint' type="button" value="print" onclick="PrintDiv('divToPrint');" />
+                        </th>
                         <th td colspan="3">Fajr</th>
         
                         <th td colspan="2">Dhuhr</th>
@@ -69,6 +89,7 @@
                 </thead>
 
                 <tbody key={index}>
+
                 <?php
                  // Return date/time info of a timestamp; then format the output
 
@@ -99,8 +120,15 @@
 
             </table>
             <script type="text/javascript">     
+            function getMonth(e){
+                
+                
+                const value = e.value;
+                document.cookie=`fcookie=${value}`; 
+        
+            }
     function PrintDiv(printThis) { 
-  
+
         var printContents = document.getElementById(printThis).innerHTML;
          var originalContents = document.body.innerHTML;
          window.onbeforeprint = (event) => {
@@ -145,7 +173,10 @@
               
                  $mydate    = wp_date("j", null, $timezone = null);
                  $monthdate =  wp_date("n", null, $timezone = null);
+              
 
+                 ?>
+                 <?php
                    foreach ($prayersettingmeta as $day) {?>
                 
 
@@ -189,11 +220,13 @@
 window.onafterprint = (event) => {
              document.body.innerHTML = originalContents;
   console.log('After print');
+
 };
             }
  </script>
 
         <?php
+               
 
            }
            return ob_get_clean();
