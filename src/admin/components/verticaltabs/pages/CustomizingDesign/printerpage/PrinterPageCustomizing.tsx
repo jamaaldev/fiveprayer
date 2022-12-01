1244a7ab2fa37a8177880f06f75de7b96a7d468e
jamaalhassan <kabarojamaal@gmail.com>
2022-12-01 21:10:09 +0000

    implimenting inputs and image uploud

--- src/admin/components/verticaltabs/pages/CustomizingDesign/printerpage/PrinterPageCustomizing.tsx
+++ src/admin/components/verticaltabs/pages/CustomizingDesign/printerpage/PrinterPageCustomizing.tsx
@@ -16,13 +16,80 @@
 // along with FivePrayer.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react'
+import styled from 'styled-components'
+import '../../../../elements/css/FPInput.scss';
 
 type Props = {}
 
-function PrinterPageCustomizing({}: Props) {
+function PrinterPageCustomizing({ }: Props) {
+const imgID =  React.useRef<HTMLImageElement>(null);
+  const ImageLoad = (file) =>{
+    console.log("📢[PrinterPageCustomizing.tsx:27]: file: ", file.target.files);
+    let [imageName] = file.target.files;
+    console.log("📢[PrinterPageCustomizing.tsx:27]: imageName: ", imageName);
+
+    if(imageName){
+        // make sure to use this ! mark if you see Object is possibly 'undefined' etc.
+        imgID.current!.src  = URL.createObjectURL(imageName);
+    }
+  }
   return (
-    <div id='printer-page' className='tabcontent'>PrinterPageCustomizing</div>
+    <div id='printer-page' className='tabcontent'>
+      Printer Page Add Extra Information
+      <PrinterContainer className='FP__input__container'>
+
+
+        <div>
+          <h1>Left Side</h1>
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+
+        </div>
+
+        <div className="profile-uploud" >
+          <form  onChange={ImageLoad}>
+          <img ref={imgID}  width="200" height="200" src="https://fakeimg.pl/200x200/" />
+          
+
+          <input type="file" className='FP__input'   name="avatar" accept="image/png, image/gif, image/jpeg" />
+
+          </form>
+
+        </div>
+
+        <div>
+          <h1>Right Side</h1>
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+          <input className='FP__input' type='text' name='Add Details' placeholder='Add Details' />
+
+        </div>
+
+      </PrinterContainer>
+    </div>
+
   )
 }
 
+const PrinterContainer = styled.div`
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        div{
+          width: 100%;
+    }
+    .profile-uploud{
+      width:200px;
+      margin: 5px;
+    }
+        `;
 export default PrinterPageCustomizing
